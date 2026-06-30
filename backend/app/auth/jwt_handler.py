548cb8b127001d3023881986b64b600dd2f40e91
Nitish <devadanitish@gmail.com>
2026-07-01 01:16:04 +0530

    Implement JWT creation and verification

---
+++ backend/app/auth/jwt_handler.py
@@ -0,0 +1,38 @@
+import os
+
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+ALGORITHM = "HS256"
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
+
+def create_accesss_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        return payload
+    except JWTError:
+        return None
